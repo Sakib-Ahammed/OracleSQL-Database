9f86fb3f30dfa539797c6e8d2509f927027955d6
Sakib Ahammed <satuhin6573@gmail.com>
2021-06-17 13:51:16 -0700

    initial commit

---
+++ create_table.sql
@@ -0,0 +1,25 @@
+-- create user/schema;
+CREATE USER user_name
+	IDENTIFIED BY password;
+-- grant permissions like create sesssion, crate table, etc
+GRANT crate session, create table ...
+to user_name;
+-- assign table space for user
+-- must be connect system
+ alter user usr_name quota 50m on system;
+
+-- show all tables in schema/user
+SELECT table_name
+	FROM user_tables
+	ORDER BY table_name;
+-- create a simple table
+CREATE TABLE students (
+  ID number(11) primary key,
+  name varchar2(60),
+  mobile varchar2(11),
+  address varchar2(200)
+  );
+  
+ -- show table describe
+ desc table_name;
+  
